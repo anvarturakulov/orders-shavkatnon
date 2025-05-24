37fa1507ff57f64fa5678f248b77ce48009bf041
Anvar Turakulov <anvarsam@mail.ru>
2025-05-24 21:13:01 +0500

    fix enter with telegram

--- frontend/app/components/auth/Auth.tsx
+++ frontend/app/components/auth/Auth.tsx
@@ -33,9 +33,8 @@ export default function Auth() {
         const tg = window.Telegram.WebApp;
         tg.ready();
         const data = tg.initData;
-        if (1) {
-          // const body = { initData: data };
-          const body = {initData: 'query_id=AAFmBTMyAAAAAGYFMzJrQq9E&user=%7B%22id%22%3A842204518%2C%22first_name%22%3A%22Anvar%22%2C%22last_name%22%3A%22Turakulov%22%2C%22username%22%3A%22AnvarTurakulov%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fi6_aXS123tuPLsylAdbFLC3WGMTg80JEnJJR_5de_Cw.svg%22%7D&auth_date=1747223696&signature=45VMXFp2uBVNNoHW5dDWGAU0EjPp0V8wCrDRjD9apaCQcL6B9UncjrB8h43Io5ytnyl39HejsQZNYUFacY7RDg&hash=91cf79ce8b7a19a37983224433d648d3dc39a5ecc3b14eb809fa90931fca43e8'}
+        if (data) {
+          const body = { initData: data };
           loginToApp(body, setMainData )
         } else {
           console.warn('initData is empty');
